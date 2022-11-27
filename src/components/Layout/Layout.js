554b1b18966e1e91f9708fa972bd741cbd8f1f41
Md. Imrul Kayes <md.tushar1804@gmail.com>
2022-11-27 23:56:41 +0600

    8.34. Adding a Hamburger Icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Auxlary from '../../hoc/Auxlary'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 // import PropTypes from 'prop-types'
-
 // Css class
 import classes from './Layout.css'
-const Layout = props => (
-    <Auxlary>
-        <Toolbar />
-        <SideDrawer />
-        <div>Toolbar, SldeDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxlary>
-)
 
-// Layout.propTypes = {}
+
+class Layout extends Component{
+
+    state = {
+        shwoSideDraer: true
+    }
+
+    sideBrawerClosedHandler = () => {
+        this.setState({shwoSideDraer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {shwoSideDraer: !prevState.shwoSideDraer}
+        })
+    }
+
+    render () {
+        return (
+            <Auxlary>
+                <Toolbar drawerToggleClicked = {this.sideDrawerToggleHandler} />
+                <SideDrawer 
+                    open = {this.state.shwoSideDraer} 
+                    closed = {this.sideBrawerClosedHandler}
+                />
+                <div>Toolbar, SldeDrawer, Backdrop</div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxlary>
+        )
+    }
+}
 
 export default Layout 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 import classes from './Toolbar.css'
 // import PropTypes from 'prop-types'
 
 const Toolbar = props => (
     <header className={classes.Toolbar}>
-        <div>MENUE</div>
+        <DrawerToggle clicked = {props.drawerToggleClicked} />
         <Logo height = '80%' />
         <nav className={classes.DasktopOnly}>
             <NavigationItems />
